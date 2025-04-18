147bf78513b04e6bcccbe200231e0793f3be7b9a
Avyukt Soni <soniavyukt@gmail.com>
2025-04-18 06:52:01 +0530

    fixed Landing page errors

--- client/components/Landing.tsx
+++ client/components/Landing.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import EncButton from "@/components/buttons/encrypt";
 import Reveal from "./ui/Reveal";
+import { LockIcon, CheckIcon } from "lucide-react";
 
 export function Landing() {
   return (
@@ -265,44 +265,5 @@ export function Landing() {
         </nav>
       </footer>
     </div>
-  );
-}
-
-function CheckIcon() {
-  return (
-    <svg
-      // {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M20 6 9 17l-5-5" />
-    </svg>
-  );
-}
-
-function LockIcon() {
-  return (
-    <svg
-      // {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <rect width="18" height="11" x="3" y="11" rx="2" ry="2" />
-      <path d="M7 11V7a5 5 0 0 1 10 0v4" />
-    </svg>
   );
 }
